0dacce3581daa12b7ac075c62835f306edf42ca2
Jisan Gain <jisangain27@gmail.com>
2024-06-10 23:13:57 +0600

    Photo send added

--- bot.py
+++ bot.py
@@ -9,26 +9,13 @@ one the user sends the bot
 """
 import asyncio
 import logging
+from genaretor import generate
 from time import time
 from game import game
-from telegram import (
-    KeyboardButton,
-    KeyboardButtonPollType,
-    Poll,
-    ReplyKeyboardMarkup,
-    ReplyKeyboardRemove,
-    Update,
-)
+from telegram import KeyboardButton, KeyboardButtonPollType, Poll, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, InlineKeyboardButton, InlineKeyboardMarkup, InputFile
 from telegram.constants import ParseMode
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-    MessageHandler,
-    PollAnswerHandler,
-    PollHandler,
-    filters,
-)
+from telegram.ext import  Application, CommandHandler, ContextTypes, MessageHandler, PollAnswerHandler, PollHandler, filters, CallbackQueryHandler
+import os
 
 # Enable logging
 logging.basicConfig(
@@ -41,7 +28,7 @@ logger = logging.getLogger(__name__)
 
 queue_players = set()
 playing = set()
-
+board_genarator = generate()
 async def create_poll(update: Update, context: ContextTypes.DEFAULT_TYPE, chat_id, question, options, wait_period) -> str:
     """Sends a predefined poll"""
     message = await context.bot.send_poll(
@@ -62,7 +49,6 @@ async def create_poll(update: Update, context: ContextTypes.DEFAULT_TYPE, chat_i
     context.bot_data.update(payload)
     return message.poll.id
 async def receive_poll_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    print(update.poll_answer)
     """Summarize a users poll vote"""
     answer = update.poll_answer
     try:
@@ -74,6 +60,7 @@ async def create_poll_and_get_answer(update: Update, context: ContextTypes.DEFAU
     # Get answer by handling create_poll and receive_poll_answer
     poll_id = await create_poll(update, context, chat_id, question, options, wait_period)
     current_time = 0
+    increment = wait_period/20
     while current_time <= wait_period:
         try:
             answer = context.bot_data[poll_id]["answer"]
@@ -84,12 +71,74 @@ async def create_poll_and_get_answer(update: Update, context: ContextTypes.DEFAU
         except:
             pass
         await asyncio.sleep(0.5)
-        current_time += 0.5
+        current_time += increment
     if delete_poll:
         await context.bot.delete_message(context.bot_data[poll_id]["chat_id"], context.bot_data[poll_id]["message_id"])
     context.bot_data.pop(poll_id)
     return -1
 
+async def create_button(update: Update, context: ContextTypes.DEFAULT_TYPE, chat_id, question, keyboard) -> int:
+    """Sends a message with three inline buttons attached."""
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    send_button = await update.message.reply_text(question, reply_markup=reply_markup)
+    payload = {
+        send_button.message_id: {
+            "chat_id": chat_id,
+        }
+    }
+    context.bot_data.update(payload)
+    return send_button.message_id
+async def receive_button_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+    try:
+        context.bot_data[query.message.message_id]["answer"] = query.data
+    except:
+        return
+    #await query.edit_message_text(text=f"You have got: {query.data}")
+async def create_button_and_get_answer(update: Update, context: ContextTypes.DEFAULT_TYPE, chat_id, question, keyboard, wait_period, delete_button = False) -> int:
+    message_id = await create_button(update, context, chat_id, question, keyboard)
+    current_time = 0
+    increment = wait_period/20
+    while current_time <= wait_period:
+        try:
+            answer = context.bot_data[message_id]["answer"]
+            context.bot_data.pop(message_id)
+            if delete_button:
+                await context.bot.delete_message(chat_id, message_id)
+            return answer
+        except:
+            pass
+        await asyncio.sleep(0.5)
+        current_time += increment
+    if delete_button:
+        await context.bot.delete_message(chat_id, message_id)
+    context.bot_data.pop(message_id)
+    return -1
+
+async def send_files(update: Update, context: ContextTypes.DEFAULT_TYPE, chat_ids, photo_loc) -> None:
+    """Send files to the specified chat IDs and delete them afterwards."""
+    try:
+        base_path = 'gena/'
+        tasks = []
+        for chat_id, file_name in zip(chat_ids, photo_loc):
+            file_path = os.path.join(base_path, file_name)
+            if os.path.exists(file_path):
+                with open(file_path, 'rb') as file:
+                    tasks.append(asyncio.create_task(context.bot.send_photo(chat_id=chat_id, photo=InputFile(file))))
+            else:
+                logger.warning(f'File {file_name} not found.')
+        await asyncio.gather(*tasks)
+        for file_name in photo_loc:
+            if os.path.exists(file_path):
+                file_path = os.path.join(base_path, file_name)
+                os.remove(file_path)
+                logger.info(f'Sent and deleted {file_name}')
+            else:
+                logger.warning(f'File {file_name} not found.')
+    except Exception as e:
+        logger.error(f'Error sending files: {e}')
+        await update.message.reply_text('Failed to send and delete files.')
 
 async def run_game(update: Update, context: ContextTypes.DEFAULT_TYPE, players, game) -> None:
     check_ready = await asyncio.gather(*[asyncio.create_task(create_poll_and_get_answer(update, context, chat_id, "Players found. Are you ready?", ["Yes", "No"], 30, True)) for chat_id in players])
@@ -98,6 +147,13 @@ async def run_game(update: Update, context: ContextTypes.DEFAULT_TYPE, players, 
     else:
         await asyncio.gather(*[asyncio.create_task(context.bot.send_message(chat_id, "Players are not ready")) for chat_id in players])
         return
+    status = game.status()
+    files = board_genarator.generate2(status[2], [0,2])
+    await send_files(update, context, players, files)
+    #keyboard = [
+    #    [InlineKeyboardButton("Roll Dice", callback_data="1")],
+    #]
+    #a = await create_button_and_get_answer(update, context, update.message.chat.id, question, keyboard, 10, True)
     return
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -105,6 +161,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_chat.id
     if chat_id not in queue_players and chat_id not in playing:
         queue_players.add(chat_id)
+        await update.message.reply_text(f"Searching for players, please wait")
         if len(queue_players) > 1:  # Fix the condition here
             players = [queue_players.pop(), queue_players.pop()]
             playing.update(players)
@@ -118,13 +175,10 @@ async def _start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 def main() -> None:
     """Run bot."""
-    # Create the Application and pass it your bot's token.
     application = Application.builder().token("1250677312:AAGvNeYI6Mn6w4MmJCgn_0IXpIrKCjgtYPY").build()
-    #application.add_handler(CommandHandler("start", start))
-    #application.add_handler(CommandHandler("poll", create_poll))
     application.add_handler(CommandHandler("start", _start))
     application.add_handler(PollAnswerHandler(receive_poll_answer))
-
+    application.add_handler(CallbackQueryHandler(receive_button_answer))
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
--- test.py
+++ test.py
@@ -2,65 +2,159 @@
 # pylint: disable=unused-argument
 # This program is dedicated to the public domain under the CC0 license.
 
-"""
-Simple Bot to handle '(my_)chat_member' updates.
-Greets new users & keeps track of which chats the bot is in.
-
-Usage:
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
+"""Basic example for a bot that can receive payment from user."""
 
 import logging
-from typing import Optional, Tuple
 
-from telegram import Chat, ChatMember, ChatMemberUpdated, Update
-from telegram.constants import ParseMode
+from telegram import LabeledPrice, ShippingOption, Update
 from telegram.ext import (
     Application,
-    ChatMemberHandler,
     CommandHandler,
     ContextTypes,
     MessageHandler,
+    PreCheckoutQueryHandler,
+    ShippingQueryHandler,
     filters,
 )
 
 # Enable logging
-
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-import asyncio
 # set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-k = 0
-async def f3(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    global k
-    print("as")
-    await asyncio.sleep(10)
-    context.bot_data.update({k:2})
-    k+=1
-    print(context, context.bot_data)
-    await asyncio.sleep(10)
-    
+PAYMENT_PROVIDER_TOKEN = "USDT"
 
-async def f1(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    print("AS")
-    asyncio.create_task(f3(update, context))
-async def f2(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    pass
+
+async def start_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Displays info on how to use the bot."""
+    msg = (
+        "Use /shipping to get an invoice for shipping-payment, or /noshipping for an "
+        "invoice without shipping."
+    )
+
+    await update.message.reply_text(msg)
+
+
+async def start_with_shipping_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends an invoice with shipping-payment."""
+    chat_id = update.message.chat_id
+    title = "Payment Example"
+    description = "Payment Example using python-telegram-bot"
+    # select a payload just for you to recognize its the donation from your bot
+    payload = "Custom-Payload"
+    # In order to get a provider_token see https://core.telegram.org/bots/payments#getting-a-token
+    currency = "USD"
+    # price in dollars
+    price = 1
+    # price * 100 so as to include 2 decimal points
+    # check https://core.telegram.org/bots/payments#supported-currencies for more details
+    prices = [LabeledPrice("Test", price * 100)]
+
+    # optionally pass need_name=True, need_phone_number=True,
+    # need_email=True, need_shipping_address=True, is_flexible=True
+    await context.bot.send_invoice(
+        chat_id,
+        title,
+        description,
+        payload,
+        PAYMENT_PROVIDER_TOKEN,
+        currency,
+        prices,
+        need_name=True,
+        need_phone_number=True,
+        need_email=True,
+        need_shipping_address=True,
+        is_flexible=True,
+    )
+
+
+async def start_without_shipping_callback(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    """Sends an invoice without shipping-payment."""
+    chat_id = update.message.chat_id
+    title = "Payment Example"
+    description = "Payment Example using python-telegram-bot"
+    # select a payload just for you to recognize its the donation from your bot
+    payload = "Custom-Payload"
+    # In order to get a provider_token see https://core.telegram.org/bots/payments#getting-a-token
+    currency = "USD"
+    # price in dollars
+    price = 1
+    # price * 100 so as to include 2 decimal points
+    prices = [LabeledPrice("Test", price * 100)]
+
+    # optionally pass need_name=True, need_phone_number=True,
+    # need_email=True, need_shipping_address=True, is_flexible=True
+    await context.bot.send_invoice(
+        chat_id, title, description, payload, PAYMENT_PROVIDER_TOKEN, currency, prices
+    )
+
+
+async def shipping_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Answers the ShippingQuery with ShippingOptions"""
+    query = update.shipping_query
+    # check the payload, is this from your bot?
+    if query.invoice_payload != "Custom-Payload":
+        # answer False pre_checkout_query
+        await query.answer(ok=False, error_message="Something went wrong...")
+        return
+
+    # First option has a single LabeledPrice
+    options = [ShippingOption("1", "Shipping Option A", [LabeledPrice("A", 100)])]
+    # second option has an array of LabeledPrice objects
+    price_list = [LabeledPrice("B1", 150), LabeledPrice("B2", 200)]
+    options.append(ShippingOption("2", "Shipping Option B", price_list))
+    await query.answer(ok=True, shipping_options=options)
+
+
+# after (optional) shipping, it's the pre-checkout
+async def precheckout_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Answers the PreQecheckoutQuery"""
+    query = update.pre_checkout_query
+    # check the payload, is this from your bot?
+    if query.invoice_payload != "Custom-Payload":
+        # answer False pre_checkout_query
+        await query.answer(ok=False, error_message="Something went wrong...")
+    else:
+        await query.answer(ok=True)
+
+
+# finally, after contacting the payment provider...
+async def successful_payment_callback(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Confirms the successful payment."""
+    # do something after successfully receiving payment?
+    await update.message.reply_text("Thank you for your payment!")
+
 
 def main() -> None:
-    """Start the bot."""
+    """Run the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token("1250677312:AAGvNeYI6Mn6w4MmJCgn_0IXpIrKCjgtYPY").build()
 
-    application.add_handler(CommandHandler("1", f1))
-    application.add_handler(CommandHandler("2", f2))
-    # Handle members joining/leaving chats.
+    # simple start function
+    application.add_handler(CommandHandler("start", start_callback))
+
+    # Add command handler to start the payment invoice
+    application.add_handler(CommandHandler("shipping", start_with_shipping_callback))
+    application.add_handler(CommandHandler("noshipping", start_without_shipping_callback))
+
+    # Optional handler if your product requires shipping
+    application.add_handler(ShippingQueryHandler(shipping_callback))
+
+    # Pre-checkout handler to final check
+    application.add_handler(PreCheckoutQueryHandler(precheckout_callback))
+
+    # Success! Notify your user!
+    application.add_handler(
+        MessageHandler(filters.SUCCESSFUL_PAYMENT, successful_payment_callback)
+    )
+
+    # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
